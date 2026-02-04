29ec8887af07cff174fdc7dbf36df78050d55a81
ddolly518 <20211109@sungshin.ac.kr>
2026-02-04 13:20:06 +0900

    [#1] feat: 전역 예외 처리 및 커스텀 에러 코드 구조 추가

---
+++ chatbot-server/src/main/java/com/example/chatbot/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.example.chatbot.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private CustomErrorCode errorCode;
+}
