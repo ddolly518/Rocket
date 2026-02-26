4587e3a08293310a9fb8ada7f4a5c69aa57dfcc7
ddolly518 <20211109@sungshin.ac.kr>
2026-02-26 20:40:53 +0900

    test: security test

--- chatbot-server/src/main/java/com/example/chatbot/service/AuthServiceImpl.java
+++ chatbot-server/src/main/java/com/example/chatbot/service/AuthServiceImpl.java
@@ -155,7 +155,7 @@ public class AuthServiceImpl implements AuthService {
         accessToken.setSecure(true); // https 환경이면 true
         accessToken.setPath("/");
         accessToken.setMaxAge(0); // 만료시간 0으로 설정 → 삭제
-        accessToken.setAttribute("SameSite", "Strict");
+        accessToken.setAttribute("SameSite", "None");
 
         // refreshToken 쿠키 삭제
         Cookie refreshToken = new Cookie("refreshToken", null);
@@ -163,7 +163,7 @@ public class AuthServiceImpl implements AuthService {
         refreshToken.setSecure(true);
         refreshToken.setPath("/auth/refresh");
         refreshToken.setMaxAge(0);
-        refreshToken.setAttribute("SameSite", "Strict");
+        refreshToken.setAttribute("SameSite", "None");
 
         response.addCookie(accessToken);
         response.addCookie(refreshToken);
